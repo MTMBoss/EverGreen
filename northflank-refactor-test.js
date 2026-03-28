8bf485b8525a718d44eeac533e284346cb8a1cdc
MTMBoss <123410872+MTMBoss@users.noreply.github.com>
2026-03-28 13:48:52 +0100

    chore: add Northflank refactor test entrypoint

---
+++ northflank-refactor-test.js
@@ -0,0 +1,7 @@
+console.log("========================================");
+console.log("EVERGREEN REFACTOR BRANCH ATTIVO");
+console.log("Branch test per Northflank eseguito con successo");
+console.log("Entry point: northflank-refactor-test.js");
+console.log("========================================");
+
+require("./src/bot/index.js");
